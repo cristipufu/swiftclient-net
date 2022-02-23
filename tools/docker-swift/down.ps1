7f159439305736d4fceafa13eda64ab2837793ef
Antoine Mangallon <antoine.mangallon@ubisoft.com>
2022-02-22 19:47:25 -0500

    Upgrade Powershell scripts with output and docker images

--- tools/docker-swift/down.ps1
+++ tools/docker-swift/down.ps1
@@ -1,5 +1,9 @@
-﻿# Stop and remove swift containers and image if any
+﻿Write-Output "Shutting down and removing containers"
+
+# Stop and remove swift containers and image if any
 docker stop SWIFT_AIO
 docker rm SWIFT_AIO
 docker rm SWIFT_DATA
 #docker rmi swift-aio
+
+Write-Output "Done"
--- tools/docker-swift/up.ps1
+++ tools/docker-swift/up.ps1
@@ -1,19 +1,21 @@
 ﻿$ErrorActionPreference = "Stop"
 
-# Build local
-docker build -t swift-aio .
+Write-Output "Building storage container"
 
 # Build storage container
 docker run -v /srv --name SWIFT_DATA busybox
 
+Write-Output "Starting Swift server"
+
 # Start swift server
-$ID = docker run --name SWIFT_AIO -d -p 8080:8080 --volumes-from SWIFT_DATA -t swift-aio
+docker run --restart=unless-stopped --name SWIFT_AIO -d -p 8080:8080 --volumes-from SWIFT_DATA -t  morrisjobke/docker-swift-onlyone
 
 # Wait for supervisord to start
 Start-Sleep -Seconds 15
 
-# print swift server logs
-docker logs $ID
+Write-Output "Testing authentication"
 
 # test auth
 Invoke-RestMethod -Method Get -Headers @{'X-Auth-User'= 'test:tester';'X-Auth-Key'='testing'} -Uri http://localhost:8080/auth/v1.0/
+
+Write-Output "Done"
